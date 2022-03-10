6afe942b75f239896be3c8840d81335edd10e2d1
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2022-03-10 09:06:04 +0700

    🔧 Fixed body undefiened ➕ Added web UI ♻ Changed code to only send one letter, to save bandwidth

--- index.js
+++ index.js
@@ -12,17 +12,29 @@ var options = {
 }
 
 var client = mqtt.connect(options);
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('auto-feeder'))
 
 client.on("connect", function () {
   console.log("succsessfully connected")
 })
 
+client.on('error', function (error) {
+  console.log(error);
+});
+
+client.on('message', function (topic, message) {
+  //Called each time a message is received
+  console.log('Received message:', topic, message.toString());
+});
+
 app.post("/feed-now", (req, res) => {
-  body_contents = req.body.content;
-  client.publish("auto-feeder/nbaf", body_content);
+  body_content = req.body.content;
+  client.publish("auto-feeder/nbaf", body_content[0]);
+  console.log(body_content)
   res.send(`your pet has been fed a ${body_content} serving`);
 })
 
